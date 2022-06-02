62021aa49af3eae9d441d2c2ed06cb709d6cbc58
chandrakantpatel <chandrakantpatel23@gmail.com>
2022-06-02 22:36:29 +0530

    Added movie info service

---
+++ movie-into-service/src/main/java/io/microservice/movieintoservice/MovieIntoServiceApplication.java
@@ -0,0 +1,13 @@
+package io.microservice.movieintoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieIntoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieIntoServiceApplication.class, args);
+	}
+
+}
